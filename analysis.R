8d77347b79c08d2db65fca04921fcba309d05944
Xiaomeng Zhu <106610647+xiaomeng-zhu@users.noreply.github.com>
2024-05-25 19:19:51 -0400

    updated scripts for analyzing exp3 in appendix

--- analysis.R
+++ analysis.R
@@ -43,6 +43,16 @@ sp_comp_all <- c("p(sg|pos_neg)>p(pl|pos_neg)",
 model_levels_original <- c("llama2-7B", "llama2-13B", "llama2-70B", "llama3-8B", "llama3-70B", "babbage-002", "davinci-002", "human")
 model_levels <- c("Llama 2 7B", "Llama 2 13B", "Llama 2 70B", "Llama 3 8B", "Llama 3 70B", "babbage-002", "davinci-002", "Human")
 
+base_vs_diff_level <- c("Implicit", "Explicit")
+base_vs_two_level <- c("Implicit", "Two")
+
+base_vs_diff_type <- c("p(sg|pos_neg)>p(sg|pos_pos)", 
+                       "p(sg|neg_pos)>p(sg|pos_pos)")
+base_vs_two_type <- c("p(sg|pos_neg)>p(sg|pos_pos)", 
+                      "p(sg|pos_neg)>p(sg|two_neg)", 
+                      "p(sg|neg_pos)>p(sg|pos_pos)",
+                      "p(sg|neg_pos)>p(sg|neg_two)")
+
 ########################### DEFINE FUNCTIONS ##########################
 analyze_model_res_an_only <- function(result_path, model_name, version) {
   d <- read_csv(result_path)
@@ -108,7 +118,23 @@ analyze_model_res <- function(result_path, model_name, version) {
                             type=="pos_pos_p>pos_pos_s" ~ "p(pl|pos_pos)>p(sg|pos_pos)",
                             type=="pos_pos_p>neg_neg_s" ~ "p(pl|pos_pos)>p(sg|neg_neg)",
                             type=="neg_pos_s>pos_neg_s" ~ "p(sg|neg_pos)>p(sg|pos_neg)",
-                            type=="neg_pos_p>pos_neg_p" ~ "p(pl|neg_pos)>p(pl|pos_neg)"
+                            type=="neg_pos_p>pos_neg_p" ~ "p(pl|neg_pos)>p(pl|pos_neg)",
+                            type=="pos_neg_s>two_neg_s" ~ "p(sg|pos_neg)>p(sg|two_neg)",
+                            type=="pos_neg_s>neg_two_s" ~ "p(sg|pos_neg)>p(sg|neg_two)",
+                            type=="neg_pos_s>two_neg_s" ~ "p(sg|neg_pos)>p(sg|two_neg)",
+                            type=="neg_pos_s>neg_two_s" ~ "p(sg|neg_pos)>p(sg|neg_two)",
+                            type=="two_neg_p>pos_neg_p" ~ "p(pl|two_neg)>p(pl|pos_neg)",
+                            type=="two_neg_p>neg_pos_p" ~ "p(pl|two_neg)>p(pl|neg_pos)",
+                            type=="neg_two_p>pos_neg_p" ~ "p(pl|neg_two)>p(pl|pos_neg)",
+                            type=="neg_two_p>neg_pos_p" ~ "p(pl|neg_two)>p(pl|neg_pos)",
+                            type=="two_neg_p>two_neg_s" ~ "p(pl|two_neg)>p(sg|two_neg)",
+                            type=="two_neg_p>neg_two_s" ~ "p(pl|two_neg)>p(sg|neg_two)",
+                            type=="neg_two_p>two_neg_s" ~ "p(pl|neg_two)>p(sg|two_neg)",
+                            type=="neg_two_p>neg_two_s" ~ "p(pl|neg_two)>p(sg|neg_two)",
+                            type=="two_neg_p>pos_pos_s" ~ "p(pl|two_neg)>p(sg|pos_pos)",
+                            type=="neg_two_p>pos_pos_s" ~ "p(pl|neg_two)>p(sg|pos_pos)",
+                            type=="pos_pos_p>two_neg_s" ~ "p(pl|pos_pos)>p(sg|two_neg)",
+                            type=="pos_pos_p>neg_two_s" ~ "p(pl|pos_pos)>p(sg|neg_two)"
     ))
   d <- d %>% mutate(model = case_when(model=="davinci-002" ~ "davinci-002",
                                       model=="babbage-002" ~ "babbage-002",
@@ -180,7 +206,7 @@ plot_singular_comparisons_across_version_an <- function(df) {
           axis.text.x = element_blank(),
           axis.ticks.x = element_blank(),
           legend.text = element_text(size=10),
-          legend.key.size = unit(0.3, 'cm')) + guides(fill = guide_legend(nrow = 1)) + 
+          legend.key.size = unit(0.3, 'cm')) + guides(fill = guide_legend(nrow = 2, byrow=TRUE)) + 
     col_scale 
     
   
@@ -231,18 +257,18 @@ model.diffs.an <- rbind(
 model.an <- rbind(model.bases.an, model.diffs.an)
 baselines.an <- rbind(model.bases.an, human.an)
 
-base.singular.an <- plot_comparisons_an(baselines.an, s_comp, model_levels, 4, 1)
-base.plural.an <- plot_comparisons_an(baselines.an, p_comp, model_levels, 3, 1)
-base.sp.an <- plot_comparisons_an(baselines.an, sp_comp, model_levels, 3, 1)
-base.singular.distance.an <- plot_comparisons_an(baselines.an, s_distance_comp, model_levels, 1, 3)
+base.singular.an <- plot_comparisons_an(baselines.an, s_comp, model_levels, 4, 2)
+base.plural.an <- plot_comparisons_an(baselines.an, p_comp, model_levels, 3, 2)
+base.sp.an <- plot_comparisons_an(baselines.an, sp_comp, model_levels, 3, 2)
+base.singular.distance.an <- plot_comparisons_an(baselines.an, s_distance_comp, model_levels, 1, 2)
 base.vs.diff.singular.an <- plot_singular_comparisons_across_version_an(model.an)
 
 # save plots
 # ggsave("plots/exp1_singular.pdf", base.singular.an, width=8, height = 2, dpi=300)
 # ggsave("plots/exp1_plural.pdf", base.plural.an, width=8, height = 2, dpi=300)
 # ggsave("plots/exp1_sp.pdf", base.sp.an, width=8, height = 2, dpi=300)
-# ggsave("plots/exp1_singular_distance.pdf", base.singular.distance.an, width=4.5, height = 3, dpi=300)
-# ggsave("plots/exp2_singular.pdf", base.vs.diff.singular.an, width=8, height = 2, dpi=300)
+# ggsave("plots/exp1_singular_distance.pdf", base.singular.distance.an, width=6, height = 4, dpi=300)
+# ggsave("plots/exp2_singular_vs_exp1.pdf", base.vs.diff.singular.an, width=8, height = 2, dpi=300)
 
 ############################ SIGNIFICANCE TESTING ##########################
 model.an.subset <- subset(model.an, 
@@ -287,6 +313,41 @@ plot_comparisons <- function(df, comp_levels, model_levels, ncol, nrow_legend) {
   
 }
 
+plot_singular_comparisons_across_version <-  function(df, version_level, types_to_compare) {
+  df_singular <- df %>% subset(type %in% types_to_compare) %>% 
+    mutate(type = factor(type, 
+                         levels = types_to_compare)) %>% 
+    mutate(model = factor(model,
+                          levels = model_levels)) %>%
+    mutate(version = factor(version,
+                            levels = version_level))
+  plot <- df_singular %>% group_by(model, sent_type, type, version) %>% 
+    summarize(accuracy=mean(correct), ci_low = ci.low(correct), ci_high = ci.high(correct)) %>%
+    ggplot(aes(x=sent_type, fill=model, y=accuracy)) + 
+    geom_bar(stat="identity", alpha=0.8, position=position_dodge()) + 
+    ylim(0,1) + 
+    xlab("Sentence Type") + 
+    ylab("% expected") + 
+    facet_wrap(~type+version, ncol = 4) + 
+    geom_hline(yintercept = 0.5, color="#666666", lty=2) + 
+    geom_errorbar(aes(ymin=accuracy - ci_low, ymax=accuracy + ci_high), 
+                  width = 0.4, 
+                  col="#666666", 
+                  position=position_dodge(width=1)) +
+    theme(legend.position="bottom", 
+          axis.title.x = element_blank(),
+          # axis.text.x = element_blank(),
+          # axis.ticks.x = element_blank(),
+          legend.text = element_text(size=10),
+          legend.key.size = unit(0.3, 'cm')) + guides(fill = guide_legend(nrow = 2, byrow=TRUE)) + 
+    col_scale 
+  
+  
+  return (plot)
+  
+}
+
+base.babbage <- analyze_model_res("results/base/base_babbage-002_accuracy_ref.csv", "babbage-002", "Implicit")
 base.davinci <- analyze_model_res("results/base/base_davinci-002_accuracy_ref.csv","davinci-002", "Implicit")
 base.llama2.7 <- analyze_model_res("results/base/base_llama2_7B_accuracy_ref.csv", "llama2-7B", "Implicit")
 base.llama2.13 <- analyze_model_res("results/base/base_llama2_13B_accuracy_ref.csv", "llama2-13B", "Implicit")
@@ -296,6 +357,7 @@ base.llama3.70 <- analyze_model_res("results/base/base_llama3_70B_accuracy_ref.c
 
 human <- analyze_model_res("human/human_accuracy.csv", "human", "Implicit")
 
+diff.babbage <- analyze_model_res("results/diff/diff_babbage-002_accuracy_ref.csv", "babbage-002", "Explicit")
 diff.davinci <- analyze_model_res("results/diff/diff_davinci-002_accuracy_ref.csv","davinci-002", "Explicit")
 diff.llama2.7 <- analyze_model_res("results/diff/diff_llama2_7B_accuracy_ref.csv", "llama2-7B", "Explicit")
 diff.llama2.13 <- analyze_model_res("results/diff/diff_llama2_13B_accuracy_ref.csv", "llama2-13B", "Explicit")
@@ -304,6 +366,7 @@ diff.llama3.8 <- analyze_model_res("results/diff/diff_llama3_8B_accuracy_ref.csv
 diff.llama3.70 <- analyze_model_res("results/diff/diff_llama3_70B_accuracy_ref.csv", "llama3-70B", "Explicit")
 
 model.bases <- rbind(
+  base.babbage,
   base.davinci,
   base.llama2.7,
   base.llama2.13,
@@ -313,6 +376,7 @@ model.bases <- rbind(
 )
 
 model.diffs <- rbind(
+  diff.babbage,
   diff.davinci,
   diff.llama2.7,
   diff.llama2.13,
@@ -321,7 +385,7 @@ model.diffs <- rbind(
   diff.llama3.70
 )
 
-model <- rbind(model.bases, model.diffs)
+model.base.vs.diff <- rbind(model.bases, model.diffs)
 baselines <- rbind(model.bases, human)
 
 base.singular <- plot_comparisons(baselines, s_comp, model_levels, 4, 1)
@@ -332,20 +396,53 @@ base.singular.distance <- plot_comparisons(baselines, s_distance_comp, model_lev
 diff.singular <- plot_comparisons(model.diffs, s_comp, model_levels, 4, 1)
 diff.plural <- plot_comparisons(model.diffs, p_comp, model_levels, 3, 1)
 diff.sp <- plot_comparisons(model.diffs, sp_comp, model_levels, 3, 1)
-diff.singular.distance <- plot_comparisons(baselines, s_distance_comp, model_levels, 1, 3)
+diff.singular.distance <- plot_comparisons(model.diffs, s_distance_comp, model_levels, 1, 3)
+base.vs.diff.singular <- plot_singular_comparisons_across_version(model.base.vs.diff, base_vs_diff_level, base_vs_diff_type)
 
 # # save plots
-# ggsave("plots/exp1_singular_alltype.pdf", base.singular, width=8, height = 2, dpi=300)
-# ggsave("plots/exp1_plural_alltype.pdf", base.plural, width=8, height = 2, dpi=300)
-# ggsave("plots/exp1_sp_alltype.pdf", base.sp, width=8, height = 2, dpi=300)
+# ggsave("plots/exp1_singular_alltype.pdf", base.singular, width=10, height = 3, dpi=300)
+# ggsave("plots/exp1_plural_alltype.pdf", base.plural, width=10, height = 3, dpi=300)
+# ggsave("plots/exp1_sp_alltype.pdf", base.sp, width=10, height = 3, dpi=300)
 # ggsave("plots/exp1_singular_distance_alltype.pdf", base.singular.distance, width=4.5, height = 3, dpi=300)
-# 
-# ggsave("plots/exp2_singular_alltype.pdf", diff.singular, width=8, height = 2, dpi=300)
-# ggsave("plots/exp2_plural_alltype.pdf", diff.plural, width=8, height = 2, dpi=300)
-# ggsave("plots/exp2_sp_alltype.pdf", diff.sp, width=8, height = 2, dpi=300)
+# # 
+# ggsave("plots/exp2_singular_alltype.pdf", diff.singular, width=10, height = 3, dpi=300)
+# ggsave("plots/exp2_plural_alltype.pdf", diff.plural, width=10, height = 3, dpi=300)
+# ggsave("plots/exp2_sp_alltype.pdf", diff.sp, width=10, height = 3, dpi=300)
 # ggsave("plots/exp2_singular_distance_alltype.pdf", diff.singular.distance, width=4.5, height = 3, dpi=300)
+# 
+# ggsave("plots/exp2_singular_vs_exp1_alltype.pdf", base.vs.diff.singular, width=10, height=3, dpi=300)
+
+# significance testing
+model.base.vs.diff.subset <- subset(model.base.vs.diff, 
+                          type=="p(sg|pos_neg)>p(sg|pos_pos)"|type=="p(sg|neg_pos)>p(sg|pos_pos)") %>% 
+  mutate(version=factor(version,
+                        levels=c("Implicit", "Explicit")))
+
+library(lme4)
+glm.model.alltype.base.vs.diff <- glmer(correct ~ version+type + (1|id), data = model.base.vs.diff.subset, family = "binomial")
+summary(glm.model.alltype.base.vs.diff)
 
 #================ VERSION: TWO ================
+two.babbage <- analyze_model_res("results/two/two_babbage-002_accuracy_ref.csv", "babbage-002", "Two")
+two.davinci <- analyze_model_res("results/two/two_davinci-002_accuracy_ref.csv","davinci-002", "Two")
+two.llama2.7 <- analyze_model_res("results/two/two_llama2_7B_accuracy_ref.csv", "llama2-7B", "Two")
+two.llama2.13 <- analyze_model_res("results/two/two_llama2_13B_accuracy_ref.csv", "llama2-13B", "Two")
+two.llama2.70 <- analyze_model_res("results/two/two_llama2_70B_accuracy_ref.csv", "llama2-70B", "Two")
+two.llama3.8 <- analyze_model_res("results/two/two_llama3_8B_accuracy_ref.csv", "llama3-8B", "Two")
+two.llama3.70 <- analyze_model_res("results/two/two_llama3_70B_accuracy_ref.csv", "llama3-70B", "Two")
+
+model.twos <- rbind(
+  two.babbage,
+  two.davinci,
+  two.llama2.7,
+  two.llama2.13,
+  two.llama2.70,
+  two.llama3.8,
+  two.llama3.70
+  )
+
+base.vs.two.singular <- plot_singular_comparisons_across_version(model.twos, base_vs_two_level, base_vs_two_type)
+
 
 
 #================ METRIC: NONREF ==============
---
+++ scripts/analyze_experiments_two.py
@@ -0,0 +1,135 @@
+import csv, json, sys
+import numpy as np
+from helper import read_csv, output_dictlist_to_csv
+
+config_f = sys.argv[1]
+METRIC = sys.argv[2]
+model_series = sys.argv[3]
+
+with open(config_f) as f:
+    config_dict = json.load(f)
+model_name = config_dict["model"]
+exp_version = config_dict["exp"]
+
+if model_series == "llama": # need to get size information for llama series
+    model_size = config_dict["size"]
+    result_path = f"results/{exp_version}/{exp_version}_{model_name}_{model_size}.csv"
+    summed_result_path = f"results/{exp_version}/{exp_version}_{model_name}_{model_size}_summed.csv"
+    accuracy_result_path = f"results/{exp_version}/{exp_version}_{model_name}_{model_size}_accuracy_{METRIC}.csv"
+else:
+    result_path = f"results/{exp_version}/{exp_version}_{model_name}.csv"
+    summed_result_path = f"results/{exp_version}/{exp_version}_{model_name}_summed.csv"
+    accuracy_result_path = f"results/{exp_version}/{exp_version}_{model_name}_accuracy_{METRIC}.csv"
+
+GROUP_SIZE = 12
+
+IDX_TO_TYPE = {
+    0:"pos_neg_s",
+    1:"pos_neg_p",
+    2:"two_neg_s",
+    3:"two_neg_p",
+    4:"pos_pos_s",
+    5:"pos_pos_p",
+    6:"neg_pos_s",
+    7:"neg_pos_p",
+    8:"neg_two_s",
+    9:"neg_two_p",
+    10:"neg_neg_s",
+    11:"neg_neg_p"
+}
+
+TYPE_TO_IDX = {
+    "pos_neg_s":0,
+    "pos_neg_p":1,
+    "two_neg_s":2,
+    "two_neg_p":3,
+    "pos_pos_s":4,
+    "pos_pos_p":5,
+    "neg_pos_s":6,
+    "neg_pos_p":7,
+    "neg_two_s":8,
+    "neg_two_p":9,
+    "neg_neg_s":10,
+    "neg_neg_p":11,
+}
+
+def read_expected_info(data_fname):
+    with open(data_fname) as f:
+        lines = [tuple(line.strip().split(">")) for line in f.readlines()]
+    return lines
+
+
+def get_continuation_prob(tokens, logprobs):
+    critical_token_idx = -1
+    for i, token in enumerate(tokens):
+        if token == "▁The" or token == "ĠThe" or token == "▁It" or token == "ĠIt" or token == " The" or token == " It":
+            critical_token_idx = i-1
+            break
+
+    continuation_log_prob = sum(logprobs[critical_token_idx:])
+    return critical_token_idx, continuation_log_prob
+
+def get_continuation_prob_all(results):
+    for row in results:
+        tokens = row["tokens"].split("|")
+        probs = [float(num) for num in row["logprobs"][1:-1].split(", ")]
+        # print(tokens, len(tokens), probs, len(probs))
+        # to_print = list(zip(tokens[1:], probs))
+        # for i, t in enumerate(to_print):
+        #     print(i+1, t)
+        critical_token_idx, continuation_prob = get_continuation_prob(tokens, probs)
+        # print("critical_token_idx", critical_token_idx)
+        row["critical_token_idx"] = critical_token_idx
+        # row["continuation_prob"] = continuation_prob
+        # convert log probabilities to just prob
+        row["continuation_prob"] = np.exp(continuation_prob)
+        row["type"] = "_".join(row["id"].split("_")[2:])
+        # break
+    return results
+
+
+def calculate_accuracy(results, comp_types):
+    accuracies = []
+    i = 0
+    
+    # iterate over all groups
+    while i <= len(results)-GROUP_SIZE:
+        sent_group = results[i:i+GROUP_SIZE]
+        
+        id_noun = "_".join(sent_group[0]["id"].split("_")[:2])
+        sent_type = "_".join(sent_group[0]["id"].split("_")[2:4])
+
+        # iterate over all possible comparisons
+        for exp, unexp in comp_types:
+            # print(exp, unexp)
+            exp_id = TYPE_TO_IDX[exp]
+            unexp_id = TYPE_TO_IDX[unexp]
+            comp = {}
+            comp["id"] = id_noun
+            comp["sent_type"] = sent_type
+            comp["type"] = exp+">"+unexp
+            
+            if sent_group[exp_id]["continuation_prob"] > sent_group[unexp_id]["continuation_prob"]:
+                comp["correct"] = 1
+            else:
+                comp["correct"] = 0
+            
+            accuracies.append(comp)
+
+
+        i = i + GROUP_SIZE
+
+    return accuracies
+
+
+
+def main():
+    comp_types = read_expected_info("scripts/expected_two.txt")
+    results = read_csv(result_path)
+    dictlist = get_continuation_prob_all(results)
+    output_dictlist_to_csv(dictlist, summed_result_path)
+    accuracies = calculate_accuracy(dictlist, comp_types)
+    output_dictlist_to_csv(accuracies, accuracy_result_path)
+
+
+main()
