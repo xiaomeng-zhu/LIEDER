653f929bc982c8d36064de36f8666502546b532c
Xiaomeng Zhu <106610647+xiaomeng-zhu@users.noreply.github.com>
2024-05-25 01:00:58 -0400

    added bash script

---
+++ my_job.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#SBATCH --job-name=my_job
+#SBATCH --time=16:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --partition gpu
+#SBATCH --gpus=2
+#SBATCH --constraint="a100-80g"
+
+module load miniconda
+conda activate the
+python scripts/experiments_llama_family_gpu.py config/two_llama2_70B.json
